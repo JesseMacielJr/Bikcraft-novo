8e69aa412b563ec54095982025e460f5da006348
Jessé Júnior <jessejuniormaciel@hotmail.com>
2022-02-06 12:35:27 -0300

    parametros-url e perguntas-frequentes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
+// Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 links.forEach((link) => {
     if (location.href.includes(link.href)) {
         link.classList.add("ativo");
     }
+})
+
+// Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search)
+
+parametros.forEach(parametro => {
+    const elemento = document.getElementById(parametro)
+    if (elemento) {
+        elemento.setAttribute("checked", true)
+    }
+    console.log(parametro)
+})
+
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button")
+
+perguntas.forEach(pergunta => {
+    pergunta.addEventListener('click', e => {
+        const controls = e.currentTarget.getAttribute("aria-controls")
+        const resposta = document.getElementById(controls)
+        resposta.classList.toggle("ativa");
+        e.currentTarget.setAttribute('aria-expanded', resposta.classList.contains("ativa"))
+
+    })
 })
